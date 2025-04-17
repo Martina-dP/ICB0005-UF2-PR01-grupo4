836f9546fbbe1295e7669acb74c14bffa9292b1f
Eva Fioen <eva.fioen@hotmail.com>
2025-04-17 10:53:58 +0200

    Metodo añadido en Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -116,6 +116,10 @@ public class Main {
             }
         }
 
+        public void slowDown(int decremento) {
+            
+        }
+
         @Override
         public String toString() {
             return "Vehiculo{" +
